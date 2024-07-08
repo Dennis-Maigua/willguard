01e9fc4d12bc18faf5c7435007040014900da4ff
Dennis-Maigua <dennis.maigua@strathmore.edu>
2024-07-08 04:00:56 +0300

    Upfate File

--- server/controllers/will.js
+++ server/controllers/will.js
@@ -25,6 +25,22 @@ exports.create = async (req, res) => {
     }
 };
 
+exports.fetchWills = async (req, res) => {
+    try {
+        const wills = await Will.find();
+
+        console.log('READ WILL SUCCESS!');
+        return res.json(wills);
+    }
+
+    catch (err) {
+        console.log('READ WILL FAILED:', err);
+        return res.status(500).json({
+            message: 'Failed to fetch will from database!'
+        });
+    }
+};
+
 exports.read = async (req, res) => {
     const { from } = req.params;
 
@@ -46,53 +62,6 @@ exports.read = async (req, res) => {
         console.log('LOAD WILL FAILED:', err);
         return res.status(500).json({
             error: 'Failed to read wills from database!'
-        });
-    }
-};
-
-exports.update = async (req, res) => {
-    const { from } = req.params;
-    const { status } = req.body;
-
-    try {
-        const will = await Will.find({ from });
-        if (!will) {
-            return res.status(401).json({
-                error: 'User not found!'
-            });
-        }
-
-        if (status) {
-            will.status = status.trim();
-        }
-
-        const updatedWill = await will.save();
-
-        // console.log('UPDATE WILL SUCCESS:', req.user);
-        console.log('UPDATE WILL SUCCESS!');
-        return res.json(updatedWill);
-    }
-
-    catch (err) {
-        console.log('UPDATE WILL FAILED:', err);
-        return res.status(500).json({
-            error: 'Failed to update will! Please try again.'
-        });
-    }
-};
-
-exports.fetchWills = async (req, res) => {
-    try {
-        const wills = await Will.find();
-
-        console.log('READ WILL SUCCESS!');
-        return res.json(wills);
-    }
-
-    catch (err) {
-        console.log('READ WILL FAILED:', err);
-        return res.status(500).json({
-            message: 'Failed to fetch will from database!'
         });
     }
 };
